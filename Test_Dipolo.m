a8bd10e29e6319b240ea91a4d815b88d696fd75c
A00838521 <a00838521@tec.mx>
2024-04-10 16:29:00 -0600

    Inputs

--- Test_Dipolo.m
+++ Test_Dipolo.m
@@ -3,25 +3,38 @@ clear; close all; clc;
 N=20;
 minX=-2;maxX=+2;
 minY=-2;maxY=+2;
+minZ=-2;maxZ=+2;
 x=linspace(minX,maxX,N);
 y=linspace(minY,maxY,N);
-[xG,yG]=meshgrid(x,y);
+z=linspace(minZ,maxZ,N);
+[xG,yG,zG]=meshgrid(x,y,z);
 %posicion de la particula negativa
-xCn=0.5; yCn=0; a=0.4;
+xCn = input('Ingrese la coordenada x de la primera partícula negativa: ');
+yCn = input('Ingrese la coordenada y de la primera partícula negativa: ');
+zCn = input('Ingrese la coordenada z de la primera partícula negativa: ');
+a=0.4; % Ancho
 %posicion de la particula postitiva
-xCp=-0.5; yCp=0;
+xCp = input('Ingrese la coordenada x de la primera partícula positiva: ');
+yCp = input('Ingrese la coordenada y de la primera partícula positiva: ');
+zCp = input('Ingrese la coordenada z de la primera partícula positiva: ');
 %campo electrico
-Qn=-20;
-Qp=+20;
-eps0 = 8.854e-12;
-kC = 1/(4*pi*eps0);
+Qn = input('Ingrese la carga negativa de la partícula: '); 
+Qn = abs(Qn) * -1;
+Qp = input('Ingrese la carga positiva de la partícula: ');
+Qp = abs(Qp) * 1;
+
+% Fórmulas
+kC = 8.99e+9;
 Rx = xG - xCn;
 Ry = yG - yCn;
-R = sqrt(Rx.^2 + Ry.^2).^3;
+Rz = zG - zCn;
+R = sqrt(Rx.^2 + Ry.^2 + Rz.^2).^3;
 Ex = kC .* Qn .* Rx ./ R;
 Ey = kC .* Qn .* Ry ./ R;
+Ez = kC .* Qn .* Rz ./ R;
 Rx = xG - xCp;
 Ry = yG - yCp;
+Rz = zG - zCp;
 R = sqrt(Rx.^2 + Ry.^2).^3;
 Ex = Ex + kC .* Qp .* Rx ./ R;
 Ey = Ey + kC .* Qp .* Ry ./ R;
@@ -31,7 +44,7 @@ u = Ex./E;
 v = Ey./E;
 
 figure();
-h=quiver(xG,yG,u,v,'autoscalefactor',0.6);
+h=quiver(xG,yG,u,v,'autoscalefactor',0.6); % Flechas
 set(h,'color',[1 0 0],'linewidth',1.2);
 axis([-1.5 1.5 -1.5 1.5]);
 axis equal;
