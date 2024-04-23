a9a8aac268b2c451600b53556e76dbcd8ce1166b
A00838521 <a00838521@tec.mx>
2024-04-22 18:46:59 -0600

    Sin flechas pero con for

--- Test_Dipolo.m
+++ Test_Dipolo.m
@@ -8,16 +8,18 @@ y=linspace(minY,maxY,N); % Generar valores de y
 [xG,yG]=meshgrid(x,y); % Crear una cuadrícula de puntos en el espacio 2D
 
 % Información de carga negativa
-xCn = 0; % Coordenada x de la partícula negativa
-yCn = 0; % Coordenada y de la partícula negativa
+xCn = 1; % Coordenada x de la partícula negativa
+yCn = -1.5; % Coordenada y de la partícula negativa
 Qn = 0; % Carga negativa de la partícula
-Qn = abs(Qn) * -1; % Asegurar carga negativa
+Qn = abs(Qn) * -1; % Asegurar carga negativa5
+NQn = 10;
 
 % Posición de la partícula positiva
 xCp = 0; % Coordenada x de la partícula positiva
-yCp = 0; % Coordenada y de la partícula positiva
+yCp = -1.5; % Coordenada y de la partícula positiva
 Qp = 0; % Carga positiva de la partícula
 Qp = abs(Qp) * 1; % Asegurar carga positiva
+NQp = 10;
 
 % Posición de la partícula central
 xC = 0; % Coordenada x de la partícula central
@@ -31,22 +33,59 @@ aParticula=0.1; % Ancho de la partícula central
 eps0 = 8.85e-12; % Permitividad del espacio libre
 kC = 1/(4*pi*eps0); % Constante de Coulomb
 
-% Gráfico 2D
-Rx = xG - xCn; % Componente x de la distancia entre cada punto y la partícula negativa
-Ry = yG - yCn; % Componente y de la distancia entre cada punto y la partícula negativa
-R = sqrt(Rx.^2 + Ry.^2).^3; % Distancia entre cada punto y la partícula negativa
-Ex = kC .* Qn .* Rx ./ R; % Componente x del campo eléctrico debido a la partícula negativa
-Ey = kC .* Qn .* Ry ./ R; % Componente y del campo eléctrico debido a la partícula negativa
-Rx = xG - xCp; % Componente x de la distancia entre cada punto y la partícula positiva
-Ry = yG - yCp; % Componente y de la distancia entre cada punto y la partícula positiva
-R = sqrt(Rx.^2 + Ry.^2).^3; % Distancia entre cada punto y la partícula positiva
-Ex = Ex + kC .* Qp .* Rx ./ R; % Componente x del campo eléctrico debido a la partícula positiva
-Ey = Ey + kC .* Qp .* Ry ./ R; % Componente y del campo eléctrico debido a la partícula positiva
+
+% Graficar la flecha
+figure();
+
+% Graficar la partícula central
+h=rectangle('Position',[xC-aParticula/2,yC-aParticula/2,aParticula,aParticula],'curvature',[1 1]); 
+set(h,'Facecolor',[0 0 0],'Edgecolor',[0 0 1]);
+
+% Graficar la partícula negativa
+for i=1:NQn
+    h=rectangle('Position',[xCn-a/2,yCn-a/2,a,a],'curvature',[1 1]); 
+    set(h,'Facecolor',[0 0 1],'Edgecolor',[0 0 1]);
+    text(xCn-0.07,yCn+0.05,'-','Color','white','FontSize',30);
+    hold on
+    
+    Rx = xG - xCn; % Componente x de la distancia entre cada punto y la partícula negativa
+    Ry = yG - yCn; % Componente y de la distancia entre cada punto y la partícula negativa
+    R = sqrt(Rx.^2 + Ry.^2).^3; % Distancia entre cada punto y la partícula negativa
+    Ex = kC .* Qn .* Rx ./ R; % Componente x del campo eléctrico debido a la partícula negativa
+    Ey = kC .* Qn .* Ry ./ R; % Componente y del campo eléctrico debido a la partícula negativa
+    
+    yCn = yCn+a;
+end
+
+% Graficar la partícula positiva
+for i=1:NQp
+    h=rectangle('Position',[xCp-a/2,yCp-a/2,a,a],'curvature',[1 1]); 
+    set(h,'Facecolor',[1 0 0],'Edgecolor',[1 0 1]);
+    text(xCp-0.1,yCp,'+','Color','white','FontSize',30);
+    hold on
+
+    Rx = xG - xCp; % Componente x de la distancia entre cada punto y la partícula positiva
+    Ry = yG - yCp; % Componente y de la distancia entre cada punto y la partícula positiva
+    R = sqrt(Rx.^2 + Ry.^2).^3; % Distancia entre cada punto y la partícula positiva
+    Ex = Ex + kC .* Qp .* Rx ./ R; % Componente x del campo eléctrico debido a la partícula positiva
+    Ey = Ey + kC .* Qp .* Ry ./ R; % Componente y del campo eléctrico debido a la partícula positiva
+
+    yCp = yCp+a;
+    
+end
+
 E = sqrt(Ex.^2 + Ey.^2); % Magnitud del campo eléctrico en cada punto
 
 % Componentes u y v
 u = Ex./E; % Componente x del vector unitario del campo eléctrico
 v = Ey./E; % Componente y del vector unitario del campo eléctrico
+
+h=quiver(xG,yG,u,v,'autoscalefactor',0.6); % Graficar las flechas en cada punto
+set(h,'color',[1 0 0],'linewidth',1.2);
+axis([-3.5 3.5 -3.5 3.5]);
+axis equal;
+box on
+hold on
 
 % Cálculo del Campo Eléctrico => Partícula Central 2
 magnitudDeCargas = sqrt(Qn^2 + Qp^2); % Magnitud de las cargas
@@ -64,8 +103,7 @@ vectorUnitarioR = vectorPosicionR/magnitudDeR; % Vector unitario en la direcció
 % Campo eléctrico de la partícula central
 E = (kC * (2*p/(r^2)) * vectorUnitarioR); % Campo eléctrico en la partícula central
 
-% Graficar la flecha
-figure();
+% Graficar las flechas
 u_avg = mean(u(:)); % Componente x promedio del vector unitario del campo eléctrico
 v_avg = mean(v(:)); % Componente y promedio del vector unitario del campo eléctrico
 
@@ -74,26 +112,7 @@ set(h,'color',[0 0 1],'linewidth',1.2);
 axis([-1.5 1.5 -1.5 1.5]);
 axis equal;
 box on
-hold on
-h=quiver(xG,yG,u,v,'autoscalefactor',0.6); % Graficar las flechas en cada punto
-set(h,'color',[1 0 0],'linewidth',1.2);
-axis([-1.5 1.5 -1.5 1.5]);
-axis equal;
-box on
 hold off
-
-% Graficar la partícula central
-h=rectangle('Position',[xC-aParticula/2,yC-aParticula/2,aParticula,aParticula],'curvature',[1 1]); 
-set(h,'Facecolor',[0 0 0],'Edgecolor',[0 0 1]);
-
-% Graficar la partícula negativa
-h=rectangle('Position',[xCn-a/2,yCn-a/2,a,a],'curvature',[1 1]); 
-set(h,'Facecolor',[0 0 1],'Edgecolor',[0 0 1]);
-text(xCn-0.07,yCn+0.05,'-','Color','white','FontSize',30);
-% Graficar la partícula positiva
-h=rectangle('Position',[xCp-a/2,yCp-a/2,a,a],'curvature',[1 1]); 
-set(h,'Facecolor',[1 0 0],'Edgecolor',[1 0 1]);
-text(xCp-0.1,yCp,'+','Color','white','FontSize',30);
 
 % Campo eléctrico de la partícula central
 disp("Coordenadas de la partícula central:"); % Mostrar las coordenadas de la partícula central
